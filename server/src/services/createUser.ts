91fc98bb5b7975fcd5246f92c127ab148f516bae
Alice Jost <alice.jost@outlook.de>
2021-12-18 17:41:41 +0100

    define function shell to create user

---
+++ server/src/services/createUser.ts
@@ -0,0 +1,19 @@
+export async function createUser(userModel, userInput: any) {
+
+}
+
+// TODO: In the future, create a Service Class that summarizes all User
+// operations, like `createUser`, etc.
+// class UserService {
+//     createUser() {
+
+//     },
+
+//     findUser() {
+
+//     },
+
+//     checkIfUserIsAvailable() {
+
+//     },
+// }
