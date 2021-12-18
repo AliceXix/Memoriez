fc8642bba096be8fcac952953c96069bd50e11be
Alice Jost <alice.jost@outlook.de>
2021-12-18 19:51:07 +0100

    write content to find user by username in db

--- server/src/services/loginUser.ts
+++ server/src/services/loginUser.ts
@@ -1,3 +1,5 @@
-export async function doesUserExists(userModel, userInput) {
+export async function doesUserExists(userModel, username) {
+    const userFromDB : any = userModel.findOne( {username} )
     
+    return userFromDB
 }
