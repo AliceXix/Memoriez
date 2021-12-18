d93dd53fbee4b0b68dd88b42f27aa011fe7e64ba
Alice Jost <alice.jost@outlook.de>
2021-12-18 18:46:12 +0100

    creat new user from input

--- server/src/services/createUser.ts
+++ server/src/services/createUser.ts
@@ -1,5 +1,9 @@
 export async function createUser(userModel, userInput: any) {
-
+    const newUser : object = userModel.create({
+        username : String = userInput.username,
+        mail : String = userInput.mail,
+    })
+    return newUser;
 }
 
 // TODO: In the future, create a Service Class that summarizes all User
