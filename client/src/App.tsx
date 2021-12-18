f266a37d23dbe3e0bef8da4fd6dd8d102db5aad9
Alice Jost <alice.jost@outlook.de>
2021-12-18 22:41:04 +0100

    set up register form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,18 +6,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Register here!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <label>Username:</label>
+          <input type="text"></input>
+          <br></br>
+          <label>Mail:</label>
+          <input type="text"></input>
+        </form>
       </header>
     </div>
   );
