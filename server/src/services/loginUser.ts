67cf3a9561b2c8880b8d4d422da0b164a9594e18
Alice Jost <alice.jost@outlook.de>
2022-01-23 23:00:37 +0100

    remove unnecessary .then in promise

--- server/src/services/loginUser.ts
+++ server/src/services/loginUser.ts
@@ -1,9 +1,7 @@
-export async function doesUserExists(userModel, userInput) {
+export function doesUserExists(userModel, userInput) {
     const userFromDB : any = userModel.findOne( {
         username: userInput.username,
         mail: userInput.mail,
-    } ).then((found) => {
-        console.log(found)
-    })
+    } )
     return userFromDB
 }
