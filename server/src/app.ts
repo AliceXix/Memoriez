31b575d64c8cff937b476722ac68633ac5cd6101
Alice Jost <alice.jost@outlook.de>
2022-02-05 12:43:45 +0100

    set up API to get user and memories when in dahsboard

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,112 +1,128 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import './db';
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import "./db";
 
 const app = express();
 const port = 3000;
 
-import { createUser } from './services/createUser';
-import { doesUserExists } from './services/loginUser';
-import { getUserFromDB } from './services/getUserFromDB';
-const User = require('./models/user.model');
+import { createUser } from "./services/createUser";
+//import { doesUserExists } from './services/loginUser';
+import { getUserFromDB } from "./services/getUserFromDB";
+import User from "./models/user.model";
+const Memory = require("./models/memory.model");
+const Person = require("./models/person.model");
 
-app.use(cors({
-	origin: '*'
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 //@ts-ignore
-app.listen(port, err => {
-	console.log("Listening and we know of cors:", cors);
-	if (err) {
-		return console.error(err);
-	}
-	return console.log(`server is listening on ${port}`);
+app.listen(port, (err) => {
+  console.log("Listening and we know of cors:", cors);
+  if (err) {
+    return console.error(err);
+  }
+  return console.log(`server is listening on ${port}`);
 });
 
-const register = app.post('/api/register', (req, res, next) => {
+const register = app.post("/api/register", (req, res, next) => {
+  // Read input from request
+  const userInput: object = req.body;
+  console.log(userInput);
 
-	// Read input from request
-	const userInput : object = req.body;
-	console.log(userInput)
+  // Create user from request
+  try {
+    createUser(User, userInput);
+  } catch (err: unknown) {
+    console.log(err);
+  }
 
-	// Create user from request
-	try {
-		createUser(User, userInput);
-	}
-	catch(err: unknown) {
-		console.log(err)
-	}
-
-	// Return response from created user
-	res.send({message: "success"})
+  // Return response from created user
+  res.send({ message: "success" });
 });
 
+const login = app.post("/api/login", async (req, res, next) => {
+  //read input from request
+  const { username, mail } = req.body;
+  console.log(`this is user ${username}, ${mail}`);
+  console.log(username);
 
+  async function getUser(userModel: any, username: any) {
+    let userId = userModel.findOne({ username });
+    return userId;
+  }
 
-const login = app.post('/api/login', async (req, res, next) => {
-	//read input from request
-	const {username, mail} = req.body;
+  let userID: any = await getUser(User, username);
 
-	//let userDB = await doesUserExists(User, userInput);
+  console.log(`this is user id ${userID}`);
+  console.log(userID._id);
 
-
-	//TEST//
-
-	
-
-	console.log(`this is user ${username}, ${mail}`)
-	console.log(username)
-
-	async function getUser(userModel:any, username:any) {
-
-		let userId = userModel.findOne({username})
-		return userId
-}
-
-let userID: any = await getUser(User, username);
-
-console.log(`this is user id ${userID}`)
-console.log(userID._id)
-
-	if (!userID) {
-		res.send({message: 'this user does not exist'})
-	} else {
-		//res.send({message: 'this worked fine'})
-		res.send({id: `${userID._id}`})
-	}
+  if (!userID) {
+    res.send({ message: "this user does not exist" });
+  } else {
+    //res.send({message: 'this worked fine'})
+    res.send({ id: `${userID._id}` });
+  }
 });
 
+app.get("/api/dashboard/:id", async (req, res, next) => {
+  //read input from request
+  const id = req.params;
 
-const getProfileInfo = app.get('api/dashboard/:id', async (req, res, next) => {
+  if (!id) {
+    res.send({ message: "something went wrong big time" });
+  } else {
+    res.send({ message: "this is your infos" });
+  }
 
-	//read input from request
-	const id = req.params;
+  console.log("this is the URLInput");
+  console.log(id.id)
 
-	console.log('this is the URLInput' + id)
-	//get the user in DB
-	//let userFromDB = await getUserFromDB(User, URLInput)
+  async function getUserById(userModel: typeof User, id: any) {
+    let user: any = userModel.findById(id);
+    return user;
+  }
 
-	async function getUserId(Usermodel:any , userid:any) {
-		let userId: any = Usermodel.findByID({userid})
-		return userId
-	}
+  let user: any = await getUserById(User, id.id);
 
-	let userId: any = await getUserId(User, id)
+  console.log("this comes from api dashboard and is the user");
+  console.log(user);
 
-	console.log("this comes from api dashboard")
-	console.log(userId)
+  //no you do not need a "getPerson" because Person is part of the property circle of User
 
-	if (!id) {
-		res.send({message: 'something went wrong big time'})
-	} else {
-		res.send({message: 'this is your dashboard'})
-	}
+  //------------------//
 
+  async function getMemories(personModel: any, id: any) {
+    let memories = personModel.find({ author: `${id}` });
+    return memories;
+  }
+
+  let memory: any = await getMemories(Memory, id.id);
+
+  console.log("this comes from api dashboard and are the memories");
+  console.log(memory);
 });
 
-// const getUserId = app.get('api/userId', async (req, res, next) => {
-// 	console.log(req)
-// })
+const getPersonDetails = app.get("/api/person/:id", async (req, res, next) => {
+  const id = req.params;
+
+    if (!id) {
+      res.send({ message: "something went wrong big time" });
+    } else {
+      res.send({ message: "this is your infos" });
+    }
+
+  async function getPersonById(model, id) {
+    let person = await model.findById({ id });
+    return person;
+  }
+
+
+
+  let personDetails = await getPersonById(Person, id);
+});
