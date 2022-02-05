c463d2cc6401f515c50b38b809772e0a4f6b24c4
Alice Jost <alice.jost@outlook.de>
2022-02-05 13:05:17 +0100

    add route to person details page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import RegisterForm from "./components/registerFrom";
 import LoginForm from './components/loginForm';
 import Dashboard from './components/dashboard';
+import PersonDetails from './components/person.details';
 
 export default function App() {
 
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<RegisterForm/>}/>
         <Route path="/login" element={<LoginForm/>}/>
         <Route path="/dashboard/:id" element={<Dashboard/>}/>
+        <Route path="/person/:id" element={<PersonDetails/>}/>
       </Routes>
   );
 }
