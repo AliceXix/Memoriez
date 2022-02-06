16bd782f7f33b7fd0606f65c9f383a5c68cc2301
Alice Jost <alice.jost@outlook.de>
2022-02-06 18:34:28 +0100

    base person widget

---
+++ client/src/components/person.widget.tsx
@@ -0,0 +1,9 @@
+export default function PersonWidget() {
+    return (
+      <>
+        <section className="widget main">
+          <h3>Name of the person</h3>
+        </section>
+      </>
+    );
+}
