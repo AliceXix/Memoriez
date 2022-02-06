52171c7f360a07d90a9213e5c24c208b2c2ff9bd
Alice Jost <alice.jost@outlook.de>
2022-02-06 19:49:02 +0100

    design login form

--- client/src/components/loginForm.tsx
+++ client/src/components/loginForm.tsx
@@ -1,14 +1,17 @@
 import * as React from "react";
 import { useNavigate } from "react-router-dom";
 
- interface LoginOutput {
-   id: string
- }
+interface LoginOutput {
+  id: string;
+}
 
 export default function LoginForm() {
   const [mail, setMail] = React.useState("");
   const [username, setUsername] = React.useState("");
-  const userInfo: {mail: string, username: string} = { mail: mail, username: username };
+  const userInfo: { mail: string; username: string } = {
+    mail: mail,
+    username: username,
+  };
   const navigate = useNavigate();
 
   async function handleLogin(userInfo: any) {
@@ -20,43 +23,42 @@ export default function LoginForm() {
       body: JSON.stringify(userInfo),
     });
     const data: LoginOutput = await fetcher.json();
-    return data
+    return data;
   }
-
 
   return (
     <>
-      <div className="App">
-        <header className="App-header">
-          <p>Login here!</p>
+      <main className="main">
+        <section className="box">
+          <h2>Login here!</h2>
           <form
             onSubmit={async (e) => {
               e.preventDefault();
               let newUrl = await handleLogin(userInfo);
-              console.log(
-                `this comes from the return statement ${newUrl.id}`
-              );
+              console.log(`this comes from the return statement ${newUrl.id}`);
               navigate(`/dashboard/${newUrl.id}`);
             }}
           >
-            <label htmlFor="username">Username:</label>
-            <input
-              id="username"
-              type={"text"}
-              onChange={(e) => setUsername(e.target.value)}
-            ></input>
-            <br></br>
-            <label htmlFor="mail">Mail:</label>
-            <input
-              id="mail"
-              type={"text"}
-              onChange={(e) => setMail(e.target.value)}
-            ></input>
-            <br></br>
-            <input type={"submit"} value={"Login"}></input>
+            <div className="input">
+              <label htmlFor="username">Username:</label>
+              <input
+                id="username"
+                type={"text"}
+                onChange={(e) => setUsername(e.target.value)}
+              ></input>
+            </div>
+            <div className="input">
+              <label htmlFor="mail">Mail:</label>
+              <input
+                id="mail"
+                type={"text"}
+                onChange={(e) => setMail(e.target.value)}
+              ></input>
+            </div>
+            <input type={"submit"} value={"Login"} className="button"></input>
           </form>
-        </header>
-      </div>
+        </section>
+      </main>
     </>
   );
 }
