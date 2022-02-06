6c25b5c9ed2e6c20557ac316a1fb2a7079a4182f
Alice Jost <alice.jost@outlook.de>
2022-02-06 18:35:56 +0100

    implement route to add-memory into app frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,15 +4,17 @@ import RegisterForm from "./components/registerFrom";
 import LoginForm from './components/loginForm';
 import Dashboard from './components/dashboard';
 import PersonDetails from './components/person.details';
+import AddMemoryForm from './components/AddMemoryForm';
 
 export default function App() {
 
   return (
-      <Routes>
-        <Route path="/" element={<RegisterForm/>}/>
-        <Route path="/login" element={<LoginForm/>}/>
-        <Route path="/dashboard/:id" element={<Dashboard/>}/>
-        <Route path="/person/:id" element={<PersonDetails/>}/>
-      </Routes>
+    <Routes>
+      <Route path="/" element={<RegisterForm />} />
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/dashboard/:id" element={<Dashboard />} />
+      <Route path="/person/:id" element={<PersonDetails />} />
+      <Route path="/add-memory" element={<AddMemoryForm />} />
+    </Routes>
   );
 }
