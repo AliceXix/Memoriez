ea99797530599779a22ab619878e6e73847780da
Alice Jost <alice.jost@outlook.de>
2022-02-13 22:52:22 +0100

    make API call to get person details from dB

--- client/src/components/person.details.tsx
+++ client/src/components/person.details.tsx
@@ -1,9 +1,41 @@
+import * as React from "react";
 import { useNavigate } from "react-router-dom";
 import MemoryWidget from "./memory.widget";
+import { useParams } from "react-router-dom";
+
+export interface personData {
+  memories: string[];
+  name: string;
+  relationship: string[];
+  _id: string;
+}
 
 export default function PersonDetails() {
 
-  const navigate = useNavigate();
+    const [person, setPerson] = React.useState<null | personData>();
+    const navigate = useNavigate();
+
+    let { id } = useParams();
+
+    async function getPersonDetails(id: any) {
+      const fetcher = await fetch(
+        `http://localhost:3000/api/person-details/${id}`,
+        {
+          method: "GET",
+        }
+      );
+      const data: personData = await fetcher.json();
+      setPerson(data);
+      return data;
+    }
+
+    React.useEffect(() => {
+      getPersonDetails(id);
+    }, [id]);
+
+    console.log(person) //returns object with properties
+    console.log(person?.name); //returns undefined
+
 
     //NEED:
     //details person aka name
@@ -15,11 +47,10 @@ return (
       <button
       className="button"
       onClick={() => {
-        navigate("/add-memory")
-        console.log("here I will make an API call")}
+        navigate(`/add-memory/${person?._id}`)}
         }>Add memory!</button>
       <section className="box">
-        <h2>Person name</h2>
+        <h2>Person name: {person?.name}</h2>
         <h5>relationship to person</h5>
       </section>
       <section className="widgets">
