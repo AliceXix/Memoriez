e2bdd16d259887d9759b07993c9ae9b98897701a
Alice Jost <alice.jost@outlook.de>
2022-02-13 22:50:34 +0100

    add button to go to memory details page

--- client/src/components/memory.widget.tsx
+++ client/src/components/memory.widget.tsx
@@ -1,9 +1,21 @@
+import { useNavigate } from "react-router-dom";
+
 export default function MemoryWidget() {
+  const navigate = useNavigate();
+
   return (
     <>
       <section className="widget main">
         <h3>Name of the memory</h3>
       </section>
+      <button
+        className="button-to-text"
+        onClick={() => {
+          navigate(`/memory-details/:id`);
+        }}
+      >
+        View card
+      </button>
     </>
   );
 }
