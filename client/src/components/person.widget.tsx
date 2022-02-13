78e274e229ba4b017671b7727b377f4ceb9ff077
Alice Jost <alice.jost@outlook.de>
2022-02-13 22:52:39 +0100

    add button to go to page of person details

--- client/src/components/person.widget.tsx
+++ client/src/components/person.widget.tsx
@@ -1,8 +1,21 @@
-export default function PersonWidget() {
+import { useNavigate } from "react-router-dom";
+
+interface PersonWidgetProps {
+  _id: string,
+  name: string,
+}
+
+export default function PersonWidget({_id, name}: PersonWidgetProps) {
+  const navigate = useNavigate();
+
+
     return (
       <>
         <section className="widget main">
-          <h3>Name of the person</h3>
+          <h3>Name of the person: {name}</h3>
+          <button
+          className="button-to-text"
+          onClick={() => {navigate(`/person/${_id}`)}}>View card</button>
         </section>
       </>
     );
