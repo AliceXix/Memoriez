2190e4841ef21cb8107e15c1341ea131d8153938
Alice Jost <alice.jost@outlook.de>
2022-02-14 23:32:08 +0100

    remove unecessary console.log

--- server/src/services/getUserFromDB.ts
+++ server/src/services/getUserFromDB.ts
@@ -1,8 +1,4 @@
 export async function getUserFromDB(userModel:any, URLInput:any) {
-
-
-    console.log(URLInput)
-
     const userInfos : any = await userModel.findById(URLInput)
 
     return userInfos;
