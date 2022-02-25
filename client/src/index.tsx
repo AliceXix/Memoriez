77190518ae78e8aa91212767abdfa876eef26f2b
Alice Jost <alice.jost@outlook.de>
2022-02-25 23:10:15 +0100

    add chakra UI to base of app

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import * as React from "react";
+
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from "@chakra-ui/react";
+
+// function App() {
+//   // 2. Wrap ChakraProvider at the root of your app
+//   return (
+//     <ChakraProvider>
+//       <App />
+//     </ChakraProvider>
+//   );
+// }
 
 ReactDOM.render(
   <Router>
-    <App />
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
   </Router>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 reportWebVitals();
