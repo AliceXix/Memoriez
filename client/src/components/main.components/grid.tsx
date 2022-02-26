2b337b7d1081956d3c416d04c0ea7943e47279f7
Alice Jost <alice.jost@outlook.de>
2022-02-26 18:29:11 +0100

    integrate components into grid

--- client/src/components/main.components/grid.tsx
+++ client/src/components/main.components/grid.tsx
@@ -1,4 +1,8 @@
 import { Grid, GridItem } from "@chakra-ui/react";
+import Header from './header';
+import BreadcrumbNav from './breadcrumb';
+import SideNav from './side.nav';
+import MemoryCreateCard from './memory.create.card';
 
 export default function GridLayout() {
   return (
@@ -9,10 +13,18 @@ export default function GridLayout() {
         templateColumns="repeat(16, 1fr)"
         gap={0} 
       >
-        <GridItem rowStart={4} colSpan={2} rowSpan={19} bg="gray" />
-        <GridItem rowStart={1} rowSpan={2} colSpan={16} bg="tomato" />
-        <GridItem rowStart={3} colSpan={3} rowSpan={1} bg="papayawhip" />
-        <GridItem rowStart={5} colStart={5} colEnd={15} colSpan={4} rowSpan={3} bg="blue" />
+          <GridItem rowStart={4} colSpan={2} rowSpan={19} bg="gray">
+            <SideNav/>
+          </GridItem>
+        <GridItem rowStart={1} rowSpan={2} colSpan={16} bg="tomato">
+            <Header/>
+        </GridItem>
+        <GridItem rowStart={3} colSpan={3} rowSpan={1} bg="papayawhip">
+            <BreadcrumbNav/>
+        </GridItem>
+        <GridItem rowStart={5} colStart={5} colEnd={15} colSpan={4} rowSpan={3} bg="blue">
+            <MemoryCreateCard/>
+        </GridItem>
       </Grid>
     </>
   );
