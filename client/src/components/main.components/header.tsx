38d6ab8e8d834826f9ee4ca3af8b26ea819c32f1
Alice Jost <alice.jost@outlook.de>
2022-02-26 19:18:11 +0100

    integrate logic/api call into header

--- client/src/components/main.components/header.tsx
+++ client/src/components/main.components/header.tsx
@@ -1,12 +1,48 @@
 import { Input } from "@chakra-ui/react";
 
+//import { useParams } from "react-router-dom";
+import * as React from "react";
+//import PersonWidget from "../person.widget";
+import { personData } from "../person.details";
+
+
+export interface userData {
+  memory: string[];
+  user: {
+    circle: personData[];
+    _id: string;
+    username: string;
+    mail: string;
+  };
+  _id: string;
+};
+
 export default function Header() {
+  const [user, setUser] = React.useState<null | userData>();
+  //const navigate = useNavigate();
+
+  //let { id } = useParams();
+  let id = "62090860481ca44282afbe08";
+
+  async function getProfileInfos(id: any) {
+    const fetcher = await fetch(`http://localhost:3000/api/dashboard/${id}`, {
+      method: "GET",
+    });
+    const data: userData = await fetcher.json();
+    setUser(data);
+    return data;
+  }
+
+    React.useEffect(() => {
+      getProfileInfos(id);
+    }, [id]);
+
   return (
     <>
       <div className="row-items">
         <h1>Memoriez</h1>
         <Input placeholder="Search" />
-        <h1>your name</h1>
+        <h1>{user?.user.username}</h1>
       </div>
     </>
   );
