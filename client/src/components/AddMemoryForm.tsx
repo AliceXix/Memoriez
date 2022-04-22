c91261144be31a7573989cc7cb03b434ceefee81
Alice Jost <alice.jost@outlook.de>
2022-04-22 21:17:05 +0200

    handle validation for adding memories

--- client/src/components/AddMemoryForm.tsx
+++ client/src/components/AddMemoryForm.tsx
@@ -1,17 +1,53 @@
 import * as React from "react";
-import { useParams } from "react-router-dom";
 
-import { Input, Button } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { userData } from "./main/side.nav";
+import { personData } from "./person.details";
+import { useQuery } from "react-query";
+
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from "@chakra-ui/react";
 
 export default function AddMemoryForm() {
   const [title, setTitle] = React.useState("");
   const [text, setText] = React.useState("");
-  const userInput: { title: string; text: string } = {
+  const [person, setPerson] = React.useState<string[]>([]);
+  const [memoryCount, setMemoryCount] = React.useState("0");
+  const [user, setUser] = React.useState<null | userData>();
+  const userInput: { title: string; text: string; person: string[] } = {
     title: title,
     text: text,
+    person: person,
   };
 
-  let { id } = useParams();
+  console.log(person.length + "length");
+
+  let userId = localStorage.getItem("userId");
+
+  const memoryCountUpdate = localStorage.setItem(
+    "memoryWidgetUpdate",
+    memoryCount
+  );
+  console.log(memoryCount);
+
+  async function getProfileInfos(id: string) {
+    const fetcher = await fetch(`http://localhost:3000/api/user/${id}`, {
+      method: "GET",
+    });
+    const data: userData = await fetcher.json();
+    setUser(data);
+    return data;
+  }
+
+  React.useEffect(() => {
+    if (userId) {
+      getProfileInfos(userId);
+    }
+  }, [userId]);
 
   async function addMemory(id: any, input: any) {
     const fetcher = await fetch(`http://localhost:3000/api/add-memory/${id}`, {
@@ -21,8 +57,21 @@ export default function AddMemoryForm() {
       method: "POST",
       body: JSON.stringify(input),
     });
-
+    console.log(input);
     return await fetcher.json();
+  }
+
+  // const queryKey = 'memory'
+  // const queryFn = addMemory(userId, userInput);
+
+  // const result = useQuery(queryKey, queryFn);
+
+  async function handleValidation() {
+    if (person.length === 0) {
+      return alert("please assign a person of your circle to this memory");
+    }
+
+    await addMemory(userId, userInput);
   }
 
   return (
@@ -30,26 +79,51 @@ export default function AddMemoryForm() {
       <form
         onSubmit={async (e) => {
           e.preventDefault();
-          await addMemory(id, userInput);
+          await handleValidation();
+          console.log("clicked");
         }}
       >
+        <Menu>
+          {({ isOpen }) => (
+            <>
+              <MenuButton isActive={isOpen} as={Button} rightIcon="v">
+                {isOpen ? "Close" : "Open"}
+              </MenuButton>
+              <MenuList>
+                {user?.user?.circle.map((elm: personData) => {
+                  if (!elm) return null;
+
+                  return (
+                    <MenuItem
+                      onClick={() => {
+                        setPerson([elm._id]);
+                      }}
+                    >
+                      {elm.name}
+                    </MenuItem>
+                  );
+                })}
+              </MenuList>
+            </>
+          )}
+        </Menu>
         <label htmlFor="title"></label>
-        <Input
+        <input
           id="title"
           type={"text"}
           placeholder="title"
           onChange={(e) => setTitle(e.target.value)}
-        ></Input>
+        ></input>
         <label htmlFor="text"></label>
-        <Input
+        <input
           id="text"
           type={"text"}
           placeholder="memory"
-          onChange={(e) => setText(e.target.value)}
-        ></Input>
-        <Button>
-          <input type={"submit"} value={"Create"}></input>
-        </Button>
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        ></input>
+        <button type="submit">Submit</button>
       </form>
     </>
   );
