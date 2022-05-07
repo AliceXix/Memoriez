f8f8585e7931a380ab708075dce526f2b6b2638d
Alice Jost <alice.jost@outlook.de>
2022-05-07 15:25:14 +0200

    create route for favorites list

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,43 +7,55 @@ import AddMemoryForm from './components/AddMemoryForm';
 import AddPersonForm from './components/addPersonForm';
 import MemoryDetails from './components/memory.details';
 import GridLayout from './components/main/grid';
+import Favorites from './components/favorites';
+
 
 export default function App() {
 
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<RegisterForm />} />
-        <Route path="/login" element={<LoginForm />} />
 
-        <Route path="/app/dashboard/:id" element={<GridLayout />} />
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<RegisterForm />} />
+                <Route path="/login" element={<LoginForm />} />
 
-        <Route
-          path="/app/person-details/:id"
-          element={
-            <GridLayout>
-              <PersonDetails />
-            </GridLayout>
-          }
-        />
-        <Route path="/app/add-memory/:id" element={<AddMemoryForm />} />
-        <Route
-          path="/app/add-person/:id"
-          element={
-            <GridLayout>
-              <AddPersonForm />
-            </GridLayout>
-          }
-        />
-        <Route
-          path="/app/memory-details/:id"
-          element={
-            <GridLayout>
-              <MemoryDetails />
-            </GridLayout>
-          }
-        />
-      </Routes>
-    </>
-  );
+                <Route path="/app/dashboard/:id" element={<GridLayout />} />
+
+                <Route
+                    path="/app/favorites/:id"
+                    element={
+                        <GridLayout>
+                            <Favorites />
+                        </GridLayout>
+                    }
+                />
+
+                <Route
+                    path="/app/person-details/:id"
+                    element={
+                        <GridLayout>
+                            <PersonDetails />
+                        </GridLayout>
+                    }
+                />
+                <Route path="/app/add-memory/:id" element={<AddMemoryForm />} />
+                <Route
+                    path="/app/add-person/:id"
+                    element={
+                        <GridLayout>
+                            <AddPersonForm />
+                        </GridLayout>
+                    }
+                />
+                <Route
+                    path="/app/memory-details/:id"
+                    element={
+                        <GridLayout>
+                            <MemoryDetails />
+                        </GridLayout>
+                    }
+                />
+            </Routes>
+        </>
+    );
 }
