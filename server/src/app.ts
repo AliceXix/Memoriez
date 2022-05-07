4c45045c3b51264bd0a7f33d418d603240d89bce
Alice Jost <alice.jost@outlook.de>
2022-05-07 15:26:25 +0200

    clean up

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,246 +1,254 @@
 import express from "express";
 import cors from "cors";
-import bodyParser, { text } from "body-parser";
+import bodyParser from "body-parser";
 import "./db";
-
-const app = express();
-const port = 3000;
-
 import User from "./models/user.model";
 import Memory from "./models/memory.model";
 import Person from "./models/person.model";
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+const app = express();
+const port = 3000;
+//TODO
 
+
+app.use(
+    cors({
+        origin: "*",
+    })
+);
 app.use(bodyParser.json());
 
 //@ts-ignore
 app.listen(port, (err) => {
-  console.log("Listening and we know of cors:", cors);
-  if (err) {
-    return console.error(err);
-  }
-  return console.log(`server is listening on ${port}`);
+    console.log("Listening and we know of cors:", cors);
+    if (err) {
+        return console.error(err);
+    }
+
+    return console.log(`server is listening on ${port}`);
 });
 
+
 const register = app.post("/api/register", async (req, res, next) => {
-  const userInput = req.body;
+    const userInput = req.body;
 
-  async function createUser(userModel: any, userInput: any) {
-    const newUser: any = await userModel.create({
-      username: userInput.username,
-      mail: userInput.mail,
-    });
 
-    return newUser;
-  }
+    async function createUser(userModel: any, userInput: any) {
+        const newUser: any = await userModel.create({
+            username: userInput.username,
+            mail: userInput.mail,
+        });
 
-  const newUser = await createUser(User, userInput)
+        return newUser;
+    }
 
-  res.send({ user: `${newUser}` });
+    const newUser = await createUser(User, userInput)
+
+    res.send({ user: `${newUser}` });
 });
 
 const login = app.post("/api/login", async (req, res, next) => {
-  const { username, mail } = req.body;
-
-  async function getUser(userModel: any, username: any) {
-    let userId = userModel.findOne({ username });
-    return userId;
-  }
-
-  let userID: any = await getUser(User, username);
+    const { username, mail } = req.body;
 
 
-  if (!userID) {
-    res.send({ message: "this user does not exist" });
-  } else {
-    res.send({ id: `${userID._id}` });
-  }
+    async function getUser(userModel: any, username: any) {
+        let userId = userModel.findOne({ username });
+
+        return userId;
+    }
+
+    let userID: any = await getUser(User, username);
+
+
+    if (!userID) {
+        res.send({ message: "this user does not exist" });
+    } else {
+        res.send({ id: `${userID._id}` });
+    }
 });
 
 const getUserInfoFromDB = app.get("/api/user/:id", async (req, res, next) => {
-  const id = req.params;
+    const id = req.params;
 
-  if (!id) {
-    res.send({ message: "something went wrong big time" });
-  }
+    if (!id) {
+        res.send({ message: "something went wrong big time" });
+    }
 
-  async function getUserById(model: typeof User, id: any) {
-    let user: any = await
-    model.findById(id)
-                    .populate('circle')
-                    .populate('favorites')
-    return user;
-  }
+    async function getUserById(model: typeof User, id: any) {
+        let user: any = await model
+            .findById(id)
+            .populate("circle")
+            .populate("favorites");
 
-  let user: any = await getUserById(User, id.id);
+        return user;
+    }
+
+    let user: any = await getUserById(User, id.id);
 
     if (!user) {
-      res.send({ message: "this user does not exist" });
+        res.send({ message: "this user does not exist" });
     }
-  
+
     res.send({ user: user });
-    
 });
 
 const getPersonDetails = app.get("/api/person-details/:id", async (req, res, next) => {
-  const id = req.params;
+    const id = req.params;
 
     if (!id) {
-      res.send({ message: "something went wrong big time" });
+        res.send({ message: "something went wrong big time" });
+    } else {
+        console.log(id.id)
     }
 
-  async function getPersonById(model: typeof Person, id: any) {
-    let person: any = await 
-    model.findById(id)
-              .populate('memories')
-    return person;
-  }
+    async function getPersonById(model: typeof Person, id: any) {
+        let person: any = await 
 
-  let person: any = await getPersonById(Person, id.id);
+        model.findById(id)
+                    .populate('memories')
 
-      if (!person) {
+        return person;
+    }
+
+    let person: any = await getPersonById(Person, id.id);
+
+    if (!person) {
         res.send({ message: "this user does not exist" });
-      }
-
-  res.send(person);
+    } else {
+        res.send(person);
+    }
 });
 
 const addMemory = app.post("/api/add-memory/:id", async (req, res, next) => {
-  const id = req.params;
-  const userInput = req.body;
+    const id = req.params;
+    const userInput = req.body;
 
-  if (!id) {
-    return res.send({ message: "something went wrong big time" });
-  }
+    if (!id) {
+        return res.send({ message: "something went wrong big time" });
+    }
 
-  if (!userInput.title) {
-    return res.send({ message: "something went wrong big time - title" });
-  }
+    if (!userInput.title) {
+        return res.send({ message: "something went wrong big time - title" });
+    }
 
-  if (!userInput.text) {
-    return res.send({ message: "something went wrong big time - text" });
-  }
+    if (!userInput.text) {
+        return res.send({ message: "something went wrong big time - text" });
+    }
 
-  if (!userInput.author) {
-    return res.send({ message: "something went wrong big time - author" });
-  }
+    if (!userInput.author) {
+        return res.send({ message: "something went wrong big time - author" });
+    }
 
-  if (!userInput.person) {
-    return res.send({ message: "something went wrong big time - person" });
-  }
+    if (!userInput.person) {
+        return res.send({ message: "something went wrong big time - person" });
+    }
 
-  async function addMemory(model, input): Promise<any> {
+    async function addMemory(model, input): Promise<any> {
+        const newMemory = await model.create({
+            title: input.title,
+            text: input.text,
+            author: input.author,
+            person: input.person,
+        });
+        
+        return newMemory;
+    }
 
-    const newMemory = await model.create({
-      title: input.title,
-      text: input.text,
-      author: input.author,
-      person: input.person,
-    });
-    return newMemory;
-  }
+    async function findPersonAndUpdateMemories(model, id): Promise<any> {
+        const newMemory = await addMemory(Memory, userInput);
+        const updatedPerson = await model.findByIdAndUpdate(id, {
+            $push: { memories: newMemory },
+        });
 
-  async function findPersonAndUpdateMemories(model, id): Promise<any> {
-    const newMemory = await addMemory(Memory, userInput);
-    const updatedPerson = await model.findByIdAndUpdate(id, {
-       $push: { memories: newMemory },
-    });
-    return updatedPerson;
-  }
+        return updatedPerson;
+    }
 
-  const updatedPerson = await findPersonAndUpdateMemories(Person, userInput.person);
+    const updatedPerson = await findPersonAndUpdateMemories(Person, userInput.person);
 
-  res.send({ update: updatedPerson });
+    res.send({ update: updatedPerson });
 });
 
 const addPerson = app.post("/api/add-person/:id", async (req, res, next) => {
-  const id = req.params;
-  const userInput = req.body;
+    const id = req.params;
+    const userInput = req.body;
 
-  if (!id) {
-    res.send({ message: "something went wrong big time" });
-  }
+    if (!id) {
+        res.send({ message: "something went wrong big time" });
+    }
 
-  async function addPerson(model, input) {
-    const newPerson = await model.create({
-      name: input.name,
-      relationship: input.relationship,
-    });
-    return newPerson;
-  }
+    async function addPerson(model, input) {
+        const newPerson = await model.create({
+            name: input.name,
+            relationship: input.relationship,
+        });
 
-  async function findUserAndUpdateCircle(model, id) {
-    const newPerson = await addPerson(Person, userInput);
-    const updatedUser = await model.findByIdAndUpdate(id, {
-      $push: { circle: newPerson },
-    });
-    return updatedUser;
-  }
+        return newPerson;
+    }
 
-  const updatedUser = await findUserAndUpdateCircle(User, id.id);
+    async function findUserAndUpdateCircle(model, id) {
+        const newPerson = await addPerson(Person, userInput);
+        const updatedUser = await model.findByIdAndUpdate(id, {
+            $push: { circle: newPerson },
+        });
 
-  res.send({ update: updatedUser });
+        return updatedUser;
+    }
+
+    const updatedUser = await findUserAndUpdateCircle(User, id.id);
+
+    res.send({ update: updatedUser });
 });
 
 const getMemoryDetails = app.get("/api/memory-details/:id", async (req, res, next) => {
-  const id =req.params;
+    const id =req.params;
 
-      if (!id) {
+    if (!id) {
         res.send({ message: "something went wrong big time" });
-      }
+    }
 
-      async function getMemoryById(model, id) {
-        let memory = await model.findById(id)
+    async function getMemoryById(model, id) {
+        let memory = await model.findById(id);
+
         return memory;
-      }
+    }
 
-      let memoryDetails = await getMemoryById(Memory, id.id);
+    let memoryDetails = await getMemoryById(Memory, id.id);
 
-      res.send(memoryDetails);
+    res.send(memoryDetails);
 })
 
 const addFavorite = app.post("/api/add-favorite/:id", async (req, res, next) => {
-  
-  const userId = req.params.id;
-
-  const memoryId = req.body.memoryId;
-  //needs to be from widget
-
-  async function addFavorite(model, input): Promise<any> {
-
-    const favorite = await model.findById(input);
-    return favorite
-  }
+    const userId = req.params.id;
+    const memoryId = req.body.memoryId;
 
 
-  async function findUserAndUpdateFavorites(model: any, id: string, memoryId: string): Promise<any> {
-    const newFavorite = await addFavorite(Memory, memoryId);
-    console.log({newFavorite})
-    const updatedUser = await model.findByIdAndUpdate(
-      id,
-      {
-        $push: { favorites: newFavorite._id },
-      },
-      {
-        new: true
-      }
+    async function addFavorite(model, input): Promise<any> {
+        const favorite = await model.findById(input);
+
+        return favorite
+    }
+
+
+    async function findUserAndUpdateFavorites(model: any, id: string, memoryId: string): Promise<any> {
+        const newFavorite = await addFavorite(Memory, memoryId);
+        const updatedUser = await model.findByIdAndUpdate(
+            id,
+            {
+                $push: { favorites: memoryId },
+            },
+            {
+                new: true,
+            }
+        );
+
+        return updatedUser;
+    }
+
+    const updatedUser = await findUserAndUpdateFavorites(
+        User,
+        userId,
+        memoryId
     );
-    console.log({ updatedUser });
-    return updatedUser;
-  }
 
-  const updatedUser = await findUserAndUpdateFavorites(
-    User,
-    userId,
-    memoryId
-  );
-  
-
- res.send({ update: updatedUser });
-
+    res.send({ update: updatedUser });
 })
