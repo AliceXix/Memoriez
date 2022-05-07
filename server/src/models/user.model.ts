a9b01ebe253a6cdf51153dfd289264dd872990ad
Alice Jost <alice.jost@outlook.de>
2022-05-07 15:26:08 +0200

    add favorite prop to user

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -1,4 +1,5 @@
 import { Schema, model } from "mongoose";
+import { MemoryType } from "./memory.model";
 import { PersonType } from "./person.model";
 
 export interface UserType {
@@ -6,25 +7,29 @@ export interface UserType {
   mail: string;
   password: string;
   circle: PersonType[];
+  favorites : MemoryType[];
 }
 
-const userSchema = new Schema<UserType>(
-    {
-        username: {
-            type: String
-        },
-        mail: {
-            type: String
-        },
-        password: {
-            type: String
-        },
-        circle : {
-            type: [{type : Schema.Types.ObjectId, ref: 'Person'}]
-        }
-    }
-);
+
+const userSchema = new Schema<UserType>({
+    username: {
+        type: String,
+    },
+    mail: {
+        type: String,
+    },
+    password: {
+        type: String,
+    },
+    circle: {
+        type: [{ type: Schema.Types.ObjectId, ref: "Person" }],
+    },
+    favorites: {
+        type: [{ type: Schema.Types.ObjectId, ref: "Memory" }],
+    },
+});
 
 const User= model<UserType>("User", userSchema);
+//TODO
 
 export default User
