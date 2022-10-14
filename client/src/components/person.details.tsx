651414eb8a97fe527ca34afdb579700d6cb61888
Alice Jost <alice.jost@outlook.de>
2022-10-14 19:06:26 +0200

    add single memory interface

--- client/src/components/person.details.tsx
+++ client/src/components/person.details.tsx
@@ -3,14 +3,15 @@ import MemoryWidget from "./memory.widget";
 import { useParams } from "react-router-dom";
 import { Grid, GridItem } from "@chakra-ui/react";
 import { Wrap, WrapItem } from "@chakra-ui/react";
+import { memoryData } from "./memory.details";
 
 export interface personData {
   memories: {
-    author: string[],
-    person: string[],
-    text: string,
-    title: string,
-    _id: string
+    author: string[];
+    person: string[];
+    text: string;
+    title: string;
+    _id: string;
   }[];
   name: string;
   relationship: string[];
@@ -21,64 +22,75 @@ export interface PersonDetailsProps {
   children?: React.ReactNode;
 }
 
+export interface singleMemoryData {
+  author: string[];
+  person: string[];
+  text: string;
+  title: string;
+  _id: string;
+}
 
 export default function PersonDetails({ children }: PersonDetailsProps) {
-    const [person, setPerson] = React.useState<null | personData>();
-    let { id } = useParams();
+  const [person, setPerson] = React.useState<null | personData>();
+  let { id } = useParams();
 
+  async function getPersonDetails(id: any) {
+    const fetcher: Response = await fetch(
+      `http://localhost:3000/api/person-details/${id}`,
+      {
+        method: "GET",
+      }
+    );
+    const data: personData = await fetcher.json();
+    setPerson(data);
 
-    async function getPersonDetails(id: any) {
-        const fetcher: Response = await fetch(
-            `http://localhost:3000/api/person-details/${id}`,
-            {
-                method: "GET",
-            }
-        );
-        const data: personData = await fetcher.json();
-        setPerson(data);
+    return data;
+  }
 
-        return data;
-    }
+  React.useEffect(() => {
+    getPersonDetails(id);
+  }, [id]);
 
-    React.useEffect(() => {
-        getPersonDetails(id);
-    }, [id]);
-
-
-return (
+  return (
     <>
-        <Grid
-            h="100%"
-            gap={1.5}
-            gridTemplateRows={` 2fr 2fr 2fr .05fr`}
-            gridTemplateColumns={` 1fr 2fr 2fr`}
-            gridTemplateAreas={[
-                `". . ."`,
-                `"..."`,
-                `"personDetails details details"
+      <Grid
+        h="100%"
+        gap={1.5}
+        gridTemplateRows={` 2fr 2fr 2fr .05fr`}
+        gridTemplateColumns={` 1fr 2fr 2fr`}
+        gridTemplateAreas={[
+          `". . ."`,
+          `"..."`,
+          `"personDetails details details"
                 "personDetails details details"
                 "personDetails details details"`,
-            ]}
-        >
-            <GridItem bg="tomato" gridArea="personDetails">
-                {person?.name}
-                {person?.relationship}
-            </GridItem>
+        ]}
+      >
+        <GridItem bg="tomato" gridArea="personDetails">
+          {person?.name}
+          {person?.relationship}
+        </GridItem>
 
-            <GridItem gridArea="details" bg="blue">
-                <div className="scrollable">
-                    <Wrap spacing="30px" bg="yellow" height="70vh">
-                        {person?.memories.map((elm) => {
-                            return (
-                                <WrapItem height="180px" width="30%" bg="purple">
-                                    <MemoryWidget key={elm._id} title={elm.title} _id={elm._id} />
-                                </WrapItem>
-                            );
-                        })}
-                    </Wrap>
-                </div>
-            </GridItem>
-        </Grid>
+        <GridItem gridArea="details" bg="blue">
+          <div className="scrollable">
+            <Wrap spacing="30px" bg="yellow" height="70vh">
+              {person?.memories.map((elm: singleMemoryData) => {
+                //TODO do I actually need to type this?
+                return (
+                  <WrapItem height="180px" width="30%" bg="purple">
+                    <MemoryWidget
+                      key={elm._id}
+                      title={elm.title}
+                      _id={elm._id}
+                      text={elm?.text}
+                    />
+                  </WrapItem>
+                );
+              })}
+            </Wrap>
+          </div>
+        </GridItem>
+      </Grid>
     </>
-);
+  );
 }
