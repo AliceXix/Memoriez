468a1312ea111b49eef816ffa39e82ba81beb5f2
Alice Jost <alice.jost@outlook.de>
2022-10-14 19:24:51 +0200

    use mutation to update fav list

--- client/src/components/memory.widget.tsx
+++ client/src/components/memory.widget.tsx
@@ -1,69 +1,64 @@
-import { NavigateFunction, useNavigate } from "react-router-dom";
-import React from "react";
-import { userData } from "./main/side.nav";
+import { useMutation, useQueryClient } from "react-query";
+import { Link } from "react-router-dom";
 
 interface MemoriesWidgetProps {
   _id: string;
   title: string;
+  text: string;
 }
 
-export default function MemoryWidget({_id, title}: MemoriesWidgetProps) {
-    const navigate: NavigateFunction = useNavigate();
-    const [user, setUser] = React.useState<null | userData>();
-    let userId: string | null = localStorage.getItem("userId");
+export default function MemoryWidget({
+  _id,
+  title,
+  text,
+}: MemoriesWidgetProps) {
+  let userData: any | null = localStorage.getItem("userId");
+  userData = JSON.parse(userData);
+  const userId: string = userData._id;
+  const queryClient = useQueryClient();
 
-    async function getProfileInfos(id: any) {
-        const fetcher: Response = await fetch(`http://localhost:3000/api/user/${id}`, {
-            method: "GET",
-        });
-        const data: userData = await fetcher.json();
-        setUser(data);
+  async function addFavorite({ userId, _id }: { userId: string; _id: string }) {
+    const fetcher: Response = await fetch(
+      `http://localhost:3000/api/add-favorite/${userId}`,
+      {
+        headers: {
+          "Content-type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify({ memoryId: _id }),
+      }
+    );
 
-        return data;
-    }
+    return await fetcher.json();
+  }
 
-    React.useEffect(() => {
-        if (userId) {
-            getProfileInfos(userId);
-        }
-    }, [userId]);
-
-
-    async function addFavorite(userId: any, memoryId: any) {
-        const fetcher: Response = await fetch(
-            `http://localhost:3000/api/add-favorite/${userId}`,
-            {
-                headers: {
-                    "Content-type": "application/json",
-                },
-                method: "POST",
-                body: JSON.stringify({memoryId: memoryId}),
-            }
-        );
-
-        return await fetcher.json();
-    }
-
+  const mutation = useMutation(addFavorite, {
+    onSuccess: () => {
+      const queryData = queryClient.getQueryData(["getUserData"]);
+      queryClient.invalidateQueries(["getUserData"]);
+    },
+  });
 
   return (
     <>
-        <section>
-            <h3>{title}</h3>
-
-            <button
-                onClick={() => {
-                navigate(`/app/memory-details/${_id}`);
-                }}
-            >
-                View card
-            </button>
-        </section>
-
-        <button onClick={async () => {
-                await addFavorite(userId, _id)
-            }}>
+      <article className="flex-base">
+        <section className="memory-widget">
+          <Link
+            className="memory-details-link"
+            to={`/app/memory-details/${_id}`}
+          />
+          <button
+            className="centered-btn"
+            onClick={() => mutation.mutate({ userId, _id })}
+          >
             🤍
-        </button>
+          </button>
+          <h3>{title}</h3>
+          <div>
+            <p>{text}</p>
+          </div>
+        </section>
+      </article>
     </>
   );
 }
