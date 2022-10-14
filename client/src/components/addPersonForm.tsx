44758d96e6f0b5750aea939aa41a2d817f46035b
Alice Jost <alice.jost@outlook.de>
2022-10-14 19:05:21 +0200

    change input form add person - library

--- client/src/components/addPersonForm.tsx
+++ client/src/components/addPersonForm.tsx
@@ -1,51 +1,72 @@
-import * as React from "react"
-import { useNavigate, useParams } from "react-router-dom";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react";
+import * as React from "react";
 
-export default function AddPersonForm() {
-    const navigate = useNavigate();
-    const [name, setName] = React.useState("");
-    const userInput: {name: string} = {name: name};
-    let { id } = useParams();
+export default function AddPersonForm(props: {
+  isOpen: boolean;
+  onClose: () => void;
+}) {
+  const [name, setName] = React.useState("");
+  const userInput: { name: string } = { name: name };
+  const user = JSON.parse(localStorage.userId);
 
-    async function addPerson(id:any, input:any) {
-        const fetcher: Response = await fetch(
-            `http://localhost:3000/api/add-person/${id}`,
-            {
-                headers: {
-                    "Content-type": "application/json",
-                },
-                method: "POST",
-                body: JSON.stringify(input),
-            }
-        );
+  async function addPerson(id: any, input: any) {
+    const fetcher: Response = await fetch(
+      `http://localhost:3000/api/add-person/${id}`,
+      {
+        headers: {
+          "Content-type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify(input),
+      }
+    );
 
-        const newPerson = await fetcher.json();
-        return newPerson;
-    }
+    const newPerson = await fetcher.json();
+    return newPerson;
+  }
 
-    
-    return (
-        <>
+  return (
+    <>
+      <Modal isOpen={props.isOpen} onClose={props.onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
             <form
-                onSubmit={async (e: React.FormEvent<HTMLFormElement>) => {
-                    e.preventDefault();
-                    await addPerson(id, userInput);
-                }}
+              onSubmit={async (e) => {
+                e.preventDefault();
+                await addPerson(user._id, userInput);
+              }}
             >
-                <label htmlFor="name">Name:</label>
-                <input
-                    id="name"
-                    type="text"
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
-                ></input>
-                <input
-                    type="submit"
-                    value="Add new person!"
-                    onClick={() => {
-                        navigate(-1)
-                    }}
-                ></input>
+              <label htmlFor="name">Name:</label>
+              <input
+                id="name"
+                type="text"
+                onChange={(e) => setName(e.target.value)}
+              ></input>
+              <Button variant="ghost" type="submit" onClick={props.onClose}>
+                Add new person!
+              </Button>
             </form>
-        </>
-    )
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={props.onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 }
