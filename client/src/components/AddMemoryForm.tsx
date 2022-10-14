05dd44a2c443693ac17b59396d2b91228285f680
Alice Jost <alice.jost@outlook.de>
2022-10-14 19:09:06 +0200

    add modal

--- client/src/components/AddMemoryForm.tsx
+++ client/src/components/AddMemoryForm.tsx
@@ -1,127 +1,173 @@
 import * as React from "react";
 import { Button } from "@chakra-ui/react";
-import { userData } from "./main/side.nav";
+import { UserData } from "./main/side.nav";
 import { personData } from "./person.details";
+import { Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
+
 import {
-    Menu,
-    MenuButton,
-    MenuList,
-    MenuItem,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
 } from "@chakra-ui/react";
 
-export default function AddMemoryForm() {
-    const [title, setTitle] = React.useState("");
-    const [text, setText] = React.useState("");
-    const [person, setPerson] = React.useState<string[]>([]);
-    const [memoryCount] = React.useState("0");
-    const [user, setUser] = React.useState<null | userData>();
-    const userInput: { title: string; text: string; person: string[] } = {
-        title: title,
-        text: text,
-        person: person,
-    };
-    let userId: string | null = localStorage.getItem("userId");
+export default function AddMemoryForm(props: {
+  isOpen: boolean;
+  onClose: () => void;
+}) {
+  const [title, setTitle] = React.useState("");
+  const [text, setText] = React.useState("");
+  const [person, setPerson] = React.useState<personData>({
+    memories: [
+      {
+        author: [""],
+        person: [""],
+        text: "",
+        title: "",
+        _id: "",
+      },
+    ],
+    name: "",
+    relationship: [""],
+    _id: "",
+  });
+  const [memoryCount] = React.useState("0");
+  const [user, setUser] = React.useState<null | UserData>();
 
-    /**updates the memory count**/
-    localStorage.setItem(
-        "memoryWidgetUpdate",
-        memoryCount
+  let userData: any = localStorage.getItem("userId");
+  userData = JSON.parse(userData);
+
+  const author = userData._id;
+
+  const userInput: {
+    title: string;
+    text: string;
+    person: personData;
+    author: string;
+  } = {
+    title: title,
+    text: text,
+    person: person,
+    author: author,
+  };
+
+  /**updates the memory count**/
+  localStorage.setItem("memoryWidgetUpdate", memoryCount);
+
+  async function getProfileInfos(id: string) {
+    const fetcher: Response = await fetch(
+      `http://localhost:3000/api/user/${id}`,
+      {
+        method: "GET",
+      }
+    );
+    const data: UserData = await fetcher.json();
+    setUser(data);
+
+    return data;
+  }
+
+  React.useEffect(() => {
+    if (userData._id) {
+      getProfileInfos(userData._id);
+    }
+  }, [userData._id]);
+
+  async function addMemory(id: any, input: any) {
+    const fetcher: Response = await fetch(
+      `http://localhost:3000/api/add-memory/${id}`,
+      {
+        headers: {
+          "Content-type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify(input),
+      }
     );
 
+    return await fetcher.json();
+  }
 
-    async function getProfileInfos(id: string) {
-        const fetcher: Response = await fetch(`http://localhost:3000/api/user/${id}`, {
-            method: "GET",
-        });
-        const data: userData = await fetcher.json();
-        setUser(data);
-
-        return data;
+  async function handleValidation() {
+    if (!person) {
+      return alert("please assign a person of your circle to this memory");
     }
+    await addMemory(userData._id, userInput);
+  }
 
-    React.useEffect(() => {
-        if (userId) {
-            getProfileInfos(userId);
-        }
-    }, [userId]);
-
-
-    async function addMemory(id: any, input: any) {
-        const fetcher: Response = await fetch(`http://localhost:3000/api/add-memory/${id}`, {
-            headers: {
-                "Content-type": "application/json",
-            },
-            method: "POST",
-            body: JSON.stringify(input),
-        });
-
-        return await fetcher.json();
-    }
-
-
-    async function handleValidation() {
-        if (person.length === 0) {
-            return alert("please assign a person of your circle to this memory");
-        }
-
-        await addMemory(userId, userInput);
-    }
-    
-
-    return (
-        <>
-            <form
+  return (
+    <>
+      <Modal isOpen={props.isOpen} onClose={props.onClose} size="md">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Modal Title</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <section className="memory-detail-widget">
+              <form
                 onSubmit={async (e: React.FormEvent<HTMLFormElement>) => {
-                    e.preventDefault();
-                    await handleValidation();
+                  e.preventDefault();
+                  await handleValidation();
                 }}
-            >
+              >
                 <Menu>
-                    {({ isOpen }: {isOpen: boolean}) => (
-                        <>
-                            <MenuButton isActive={isOpen} as={Button} rightIcon="v">
-                                {isOpen ? "Close" : "Open"}
-                            </MenuButton>
+                  {({ isOpen }: { isOpen: boolean }) => (
+                    <>
+                      <MenuButton isActive={isOpen} as={Button} rightIcon="v">
+                        {isOpen ? "Close" : "Open"}
+                      </MenuButton>
 
-                            <MenuList>
-                                {user?.user?.circle.map((elm: personData) => {
-                                    if (!elm) return null
-                                    return (
-                                        <MenuItem
-                                            onClick={() => {
-                                                setPerson([elm._id]);
-                                            }}
-                                        >
-                                            {elm.name}
-                                        </MenuItem>
-                                    );
-                                })}
-                            </MenuList>
-                        </>
-                    )}
+                      <MenuList>
+                        {user?.user?.circle.map((elm: personData) => {
+                          if (!elm) return null;
+                          return (
+                            <MenuItem
+                              onClick={() => {
+                                setPerson(elm);
+                              }}
+                            >
+                              {elm.name}
+                            </MenuItem>
+                          );
+                        })}
+                      </MenuList>
+                    </>
+                  )}
                 </Menu>
 
-                <label htmlFor="title"></label>
-                <input
+                <div className="addmemoryform-title">
+                  <label htmlFor="title"></label>
+                  <input
                     id="title"
                     type="text"
                     placeholder="title"
                     onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                        setTitle(e.target.value)
+                      setTitle(e.target.value)
                     }
-                ></input>
+                  ></input>
+                </div>
 
                 <label htmlFor="text"></label>
-                <input
-                    id="text"
-                    type="text"
-                    placeholder="memory"
-                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                        setText(e.target.value);
-                    }}
-                ></input>
-                <button type="submit">Submit</button>
-            </form>
-        </>
-    );
+
+                <textarea
+                  id="text"
+                  name="text"
+                  placeholder="memory"
+                  onChange={(e) => {
+                    setText(e.target.value);
+                  }}
+                ></textarea>
+
+                <Button variant="ghost" type="submit" onClick={props.onClose}>
+                  Submit
+                </Button>
+              </form>
+            </section>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 }
