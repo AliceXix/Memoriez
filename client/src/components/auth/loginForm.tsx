ca1eec625379a3e28120db6de12f6fe2988875db
Alice Jost <alice.jost@outlook.de>
2022-10-14 19:07:17 +0200

    add css classes

--- client/src/components/auth/loginForm.tsx
+++ client/src/components/auth/loginForm.tsx
@@ -1,102 +1,102 @@
 import * as React from "react";
 import { useNavigate } from "react-router-dom";
+import { memoryData } from "../memory.details";
+import { personData } from "../person.details";
 
 interface LoginOutput {
-    id: string;
+	_id: string;
+	mail: string;
+	username: string;
+	favorites: memoryData[];
+	circle: personData[];
+	memories: memoryData[];
 }
 
 export default function LoginForm() {
-    const [mail, setMail] = React.useState("");
-    const [username, setUsername] = React.useState("");
-    const userInfo: { mail: string; username: string } = {
-        mail: mail,
-        username: username,
-    };
-    const navigate = useNavigate();
+	const [mail, setMail] = React.useState("");
+	const [username, setUsername] = React.useState("");
+	const userInfo: { mail: string; username: string } = {
+		mail: mail,
+		username: username,
+	};
+	const navigate = useNavigate();
 
+	async function handleLogin(userInfo: any): Promise<LoginOutput> {
+		const fetcher = await fetch("http://localhost:3000/api/login", {
+			headers: {
+				"Content-type": "application/json",
+			},
+			method: "POST",
+			body: JSON.stringify(userInfo),
+		});
+		const userData: LoginOutput = await fetcher.json();
+		return userData;
+	}
 
-    async function handleLogin(userInfo: any) {
-        const fetcher = await fetch("http://localhost:3000/api/login", {
-            headers: {
-                "Content-type": "application/json",
-            },
-            method: "POST",
-            body: JSON.stringify(userInfo),
-        });
-        const data: LoginOutput = await fetcher.json();
+	async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+		e.preventDefault();
 
-        return data;
-    }
+		let userData = await handleLogin(userInfo);
+		localStorage.setItem("userId", JSON.stringify(userData));
+		navigate(`/app/dashboard/${userData._id}`);
+	}
 
+	return (
+		<>
+			<main className="auth-wrapper">
+				<header>
+					<h1>Memoriez</h1>
+					<a href="/">Register</a>
+				</header>
 
-    async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault();
+				<div>
+					<div>
+						<h2>Other component</h2>
+						<p>App explanation</p>
+					</div>
+				</div>
 
-        let newUrl: LoginOutput = await handleLogin(userInfo);
+				<div>
+					<div>
+						<main>
+							<section>
+								<h1>Login here!</h1>
+								<form
+									onSubmit={async (e) => {
+									handleSubmit(e);
+									}}
+									>
+									<div>
+										<label htmlFor="username">Username:</label>
+										<input
+											id="username"
+											type="text"
+											onChange={(e) => setUsername(e.target.value)}
+											></input>
+									</div>
 
-        localStorage.setItem("userId", newUrl.id);
-        navigate(`/app/dashboard/${newUrl.id}`);
-    }
+									<div>
+										<label htmlFor="mail">Mail:</label>
+										<input
+											id="mail"
+											type="text"
+											onChange={(e) => setMail(e.target.value)}
+											></input>
+									</div>
 
-    
-    return (
-      <>
-        <header className="row-items">
-          <h1>Memoriez</h1>
-          <a href="/">Register</a>
-        </header>
-
-        <div className="split left">
-          <div className="centered">
-            <h2>Other component</h2>
-            <p>App explanation</p>
-          </div>
-        </div>
-
-        <div className="split right">
-          <div className="centered">
-            <main>
-              <section>
-                <h1>Login here!</h1>
-                <form
-                  onSubmit={async (e) => {
-                    handleSubmit(e);
-                  }}
-                >
-                    <div className="input">
-                        <label htmlFor="username">Username:</label>
-                        <input
-                            id="username"
-                            type="text"
-                            onChange={(e) =>
-                            setUsername(e.target.value)
-                            }
-                        ></input>
-                    </div>
-
-                    <div className="input">
-                        <label htmlFor="mail">Mail:</label>
-                        <input
-                            id="mail"
-                            type="text"
-                            onChange={(e) =>
-                            setMail(e.target.value)
-                            }
-                        ></input>
-                    </div>
-
-                    <div className="simple-center">
-                        <input
-                            type="submit"
-                            value="Login"
-                            className="button"
-                        ></input>
-                    </div>
-                </form>
-              </section>
-            </main>
-          </div>
-        </div>
-      </>
-    );
+									<div>
+										<input
+											type="submit"
+											value="Login"
+											className="button"
+											></input>
+									</div>
+								</form>
+							</section>
+						</main>
+					</div>
+				</div>
+			</main>
+		</>
+  );
 }
